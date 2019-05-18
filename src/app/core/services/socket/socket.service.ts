45acabd7358c0c213d7765ac2141513c52c03067
timadevelop <timadevelop@gmail.com>
2019-05-18 15:15:35 +0300

    auto-reconnecting WebSocket

--- src/app/core/services/socket/socket.service.ts
+++ src/app/core/services/socket/socket.service.ts
@@ -2,6 +2,8 @@ import { Injectable, OnDestroy } from "@angular/core";
 import { Observable, Observer, Subject } from 'rxjs';
 import { AuthService } from '../../../modules/auth/auth.service';
 
+const RECONNECTING_INTERVAL = 3000;
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,7 +17,6 @@ export class SocketService implements OnDestroy {
     if (this.subject) this.subject.complete();
   }
 
-  // TODO : Adopt to multiple subjects
   private subject: Subject<MessageEvent>;
 
   public connect(url): Subject<MessageEvent> {
@@ -25,33 +26,48 @@ export class SocketService implements OnDestroy {
 
     if (!this.subject) {
       this.subject = this.create(url);
-      console.log("Successfully connected: " + url);
     }
     return this.subject;
   }
 
   private create(url): Subject<MessageEvent> {
+    // get token
     const token = this.authService.getTokenInfo();
     if (!token || !this.authService.isLoggedIn) {
       console.warn("Cannot create secured socket for anonymous user.");
       return;
     }
 
+    // setup reconnect function
+    const that = this;
+    const reconnect = (e) => {
+      console.warn('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+      setTimeout(() => {
+        that.connect(url);
+      }, RECONNECTING_INTERVAL);
+    }
+
+    // try to open ws
     const protocol = token.access_token;
     let ws = new WebSocket(url, protocol);
-
-    const that = this;
-    ws.onclose = (e) => {
-      console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
-      setTimeout(() => {
-        that.create(url);
-      }, 1000);
+    // reconnect on close
+    ws.onclose = reconnect;
+    // log on open
+    ws.onopen = (e) => {
+      if (ws.OPEN) {
+        console.log("Successfully connected: " + url);
+      }
     }
+
+    // setup subject
 
     let observable = Observable.create((obs: Observer<MessageEvent>) => {
       ws.onmessage = obs.next.bind(obs);
       ws.onerror = obs.error.bind(obs);
-      ws.onclose = obs.complete.bind(obs);
+      ws.onclose = (e) => {
+        obs.complete.call(obs);
+        reconnect(e);
+      }
       return ws.close.bind(ws);
     });
 
