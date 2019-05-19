4439fe7abefabc91c8c3a4eb4721c6e4b8b04cd1
timadevelop <timadevelop@gmail.com>
2019-05-19 13:13:24 +0300

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,23 +51,16 @@ registerLocaleData(en);
     AppRoutingModule,
   ],
   providers: [
-    // format of translations that you use
     { provide: TRANSLATIONS_FORMAT, useValue: "xlf" },
-    // the translations that you need to load on your own
-    // { provide: TRANSLATIONS, useValue: Xliff },
-    // locale id that you're using (default en-US)
     { provide: LOCALE_ID, useValue: DEFAULT_LOCALE },
-    // optional, defines how error will be handled
     { provide: MISSING_TRANSLATION_STRATEGY, useValue: MissingTranslationStrategy.Warning },
     {
       provide: TRANSLATIONS,
       useFactory: (locale) => {
         locale = locale || DEFAULT_LOCALE;
         if (locale == DEFAULT_LOCALE) {
-          console.log('default');
           return '';
         }
-        console.log('hereeeee');
         return require(`raw-loader!../locale/messages.${locale}.xlf`);
       },
       deps: [LOCALE_ID]
