e0cdeb9c2ac40e10c05fbda265542216fc4bf2f0
timadevelop <timadevelop@gmail.com>
2019-05-19 14:44:19 +0300

    move custom icon literals to separated file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { RouterOutlet, Router, Event, NavigationStart, NavigationEnd, Navigation
 import { NzIconService } from 'ng-zorro-antd';
 import { RealtimeNotificationsService } from './core/services/socket/realtime-notifications.service';
 
-const ngZorroIconLiteral =
-  '<svg viewBox="0 0 8 14" xmlns="http://www.w3.org/2000/svg"><path d="M7.2 2.323H5.923c-1.046 0-1.278.464-1.278 1.16V5.11h2.44l-.35 2.438h-2.09v6.387H2.09V7.548H0V5.11h2.09V3.252C2.09 1.162 3.368 0 5.342 0c.93 0 1.742.116 1.858.116v2.207z" fill="#4267B2" fill-rule="evenodd"></path></svg>';
+import icons from './icons';
 
 @Component({
   selector: 'app-root',
@@ -41,9 +40,11 @@ export class AppComponent {
           }
         }
       });
-      // icons
-      this._iconService.addIconLiteral('ng-vlicon:facebook', ngZorroIconLiteral);
-      // notifications service
+      // register custom icons
+      icons.forEach(icon => {
+        this._iconService.addIconLiteral(icon.type, icon.literal);
+      });
+      // run notifications service
       this.rns.run();
   }
 
