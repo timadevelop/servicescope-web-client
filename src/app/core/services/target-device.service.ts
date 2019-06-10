502b70e03268bffdb82b31871d53be4bd91bbf34
timadevelop <timadevelop@gmail.com>
2019-06-10 18:02:30 +0300

    fix target device service (listed window resize)

--- src/app/core/services/target-device.service.ts
+++ src/app/core/services/target-device.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TargetDeviceService {
   private isMobileResolution: boolean;
-
+  resizeObservable$;
+  resizeSubscription$;
   constructor() {
+    this.updateStatus();
+    this.resizeObservable$ = fromEvent(window, 'resize')
+    this.resizeSubscription$ = this.resizeObservable$.subscribe(evt => {
+      this.updateStatus();
+    })
+  }
+
+  private updateStatus() {
     if (window.innerWidth < 1100) { // see variables.scss
       this.isMobileResolution = true;
     } else {
