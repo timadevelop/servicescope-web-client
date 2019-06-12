c182d42b9b6283d44a85c7417dc5aa32eadab236
timadevelop <timadevelop@gmail.com>
2019-06-12 18:11:40 +0300

    fixes for prod

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo $ENV env;
 npm install
 
 if [ "$ENV" = "production" ]; then
-    ng serve --host 0.0.0.0
+    ng serve --host 0.0.0.0 --disable-host-check --disableHostCheck
 else
-    ng serve --host 0.0.0.0
+    ng serve --host 0.0.0.0 --disable-host-check --disableHostCheck --base-href=/saas_web/ --public-host=https://demo.brainhub.co/saas_web
 fi
