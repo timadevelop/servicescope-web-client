9309f701b857572bed4b7c75a5ebf7342cfd4ac6
timadevelop <timadevelop@gmail.com>
2019-06-18 17:59:57 +0300

    own swipe with arrows

--- src/app/modules/shared/components/detail-view-only/carousel/carousel.component.ts
+++ src/app/modules/shared/components/detail-view-only/carousel/carousel.component.ts
@@ -71,4 +71,32 @@ export class CarouselComponent implements OnInit, AfterViewInit {
       this.carousel.goTo(this.imageIndex);
     }
   }
+
+  private swipeCoord?: [number, number];
+  private swipeTime?: number;
+
+  swipe(e: TouchEvent, when: string): void {
+    const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
+    const time = new Date().getTime();
+
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } else if (when === 'end') {
+      const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
+      const duration = time - this.swipeTime;
+
+      if (duration < 1000 //
+        && Math.abs(direction[0]) > 30 // Long enough
+        && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) { // Horizontal enough
+        const swipe = direction[0] < 0 ? 'next' : 'previous';
+        // Do whatever you want with swipe
+        if (swipe == 'next') {
+          this.carousel.next();
+        } else if (swipe == 'previous') {
+          this.carousel.pre();
+        }
+      }
+    }
+  }
 }
