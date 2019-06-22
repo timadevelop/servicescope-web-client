c5f258bd01ad3951c006a5bd87522935a07d4d40
timadevelop <timadevelop@gmail.com>
2019-06-22 19:45:03 +0300

    rm unused location fields

--- src/app/core/models/Location.model.ts
+++ src/app/core/models/Location.model.ts
@@ -2,20 +2,20 @@ import { Base } from './Base.model';
 
 export class District extends Base {
   oblast: string;
-  ekatte: string;
+  // ekatte: string;
   name: string;
-  region: string;
+  // region: string;
 }
 
 export class Location extends Base {
-  ekatte: string;
+  // ekatte: string;
   t_v_m: string;
   name: string;
-  oblast: string;
-  obstina: string;
-  kmetstvo: string;
-  kind: number;
-  category: number;
-  altitude: number;
+  // oblast: string;
+  // obstina: string;
+  // kmetstvo: string;
+  // kind: number;
+  // category: number;
+  // altitude: number;
   district: District;
 }
