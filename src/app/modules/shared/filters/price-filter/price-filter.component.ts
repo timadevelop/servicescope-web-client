f69015eb8b6bb81ebcdd85a9a746c0c2c89f08af
timadevelop <timadevelop@gmail.com>
2019-10-22 20:53:28 +0300

    fix price filter

--- src/app/modules/shared/filters/price-filter/price-filter.component.ts
+++ src/app/modules/shared/filters/price-filter/price-filter.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Params, ActivatedRoute, Router } from '@angular/router';
 
+const INITIAL_MAX_PRICE = 300;
+
 @Component({
   selector: 'app-price-filter',
   templateUrl: './price-filter.component.html',
@@ -9,7 +11,7 @@ import { Params, ActivatedRoute, Router } from '@angular/router';
 export class PriceFilterComponent implements OnInit {
 
   @Input() min: number = 0;
-  @Input() max: number = 300;
+  @Input() max: number = INITIAL_MAX_PRICE;
   @Input() currency: string = 'BGN';
 
   priceMin: number;
@@ -25,19 +27,19 @@ export class PriceFilterComponent implements OnInit {
     this.route.queryParamMap.subscribe(params => {
       this.priceMin = params.get('price_min') === null ? null : +params.get('price_min');
       this.priceMax = params.get('price_max') === null ? null : +params.get('price_max');
-      this.adjustMaxValue(this.priceMax);
+      this.adjustSliderValues(this.priceMax);
     });
   }
 
   onSliderEvent(range: [number, number]) {
     this.priceMin = range[0];
     this.priceMax = range[1];
-    this.adjustMaxValue();
+    this.adjustSliderValues();
   }
 
   onMaxChange(v: number) {
     this.priceMax = v;
-    this.adjustMaxValue(v);
+    this.adjustSliderValues(v);
   }
 
 
@@ -47,13 +49,16 @@ export class PriceFilterComponent implements OnInit {
     } else {
       this.priceMin = v;
     }
-    this.adjustMaxValue();
+    this.adjustSliderValues();
   }
 
-  adjustMaxValue(selectedMax: number = null) {
+  adjustSliderValues(selectedMax: number = null) {
     let v = selectedMax || this.priceMax;
 
-    if (v > this.max) {
+    // adjust max
+    if (!v) {
+      this.max = INITIAL_MAX_PRICE;
+    } else if (v > this.max) {
       // increase if overflow
       this.max = v * 1.5;
     } else if (this.max - v < this.max * 0.15) {
@@ -63,12 +68,28 @@ export class PriceFilterComponent implements OnInit {
       // auto decrease max value if priceMax is in first 10% of allowed range
       this.max = this.priceMax * 1.5;
     }
+
+    // adjust min
+    if (!this.priceMin) {
+      this.min = 0;
+    }
+
+    // adjust small middle
     const diff = this.priceMax - this.priceMin
     if (diff > 0 && diff < this.max * 0.25) {
       const delta = diff > 5 ? diff * 0.25 : 2;
-      if (this.priceMin - delta > 0) this.min = this.priceMin - delta;
+
       this.max = this.priceMax + delta;
+
+      if (!this.priceMin) {
+        //
+      } else if (this.min > this.priceMin) {
+        this.min = this.priceMin;
+      } else if (this.priceMin - delta > 0) {
+        this.min = this.priceMin - delta;
+      }
     }
+
   }
 
   generateMarks() {
@@ -98,6 +119,7 @@ export class PriceFilterComponent implements OnInit {
   clear() {
     this.priceMin = null;
     this.priceMax = null;
+    this.adjustSliderValues();
     this.filter();
   }
 
