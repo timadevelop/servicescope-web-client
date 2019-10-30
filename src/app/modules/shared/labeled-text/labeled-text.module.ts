c9928f7114ff54300e38d3ac7c2f73cac385bb81
timadevelop <timadevelop@gmail.com>
2019-10-30 19:19:48 +0200

    minor fixes

--- src/app/modules/shared/labeled-text/labeled-text.module.ts
+++ src/app/modules/shared/labeled-text/labeled-text.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LabeledTextComponent } from './labeled-text.component';
-import { NzInputModule } from 'ng-zorro-antd';
+import { NzInputModule, NzIconModule } from 'ng-zorro-antd';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    NzInputModule
+    NzInputModule,
+    NzIconModule
   ],
   declarations: [LabeledTextComponent],
   exports: [LabeledTextComponent]
