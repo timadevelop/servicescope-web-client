a391d47236690d2fb3d2d8d821ec77e15f35f082
timadevelop <timadevelop@gmail.com>
2020-06-20 17:12:29 +0300

    user env variables for base hrefs & locale

--- run.sh
+++ run.sh
@@ -6,13 +6,19 @@ npm install
 npx npm-force-resolutions
 npm install
 
-
 bash ./scripts/parse_env.sh
 
 if [ "$ENV" = "production" ]; then
-  #ng serve --configuration=production --aot --host 0.0.0.0 --disable-host-check --disableHostCheck --base-href=/saas_web/ --public-host=$WEBCLIENT_PUBLIC_URL
   # Angular use universal for prod
-  npm run build:ssr && npm run serve:ssr
+  if [ "$LOCALE" = "bg" ]; then
+    npm run build:ssr-bg && npm run serve:ssr
+  else
+    npm run build:ssr && npm run serve:ssr
+  fi
 else
-  ng serve --aot --host 0.0.0.0 --disable-host-check --disableHostCheck --base-href=/saas_web/ --public-host=$WEBCLIENT_PUBLIC_URL
+  if [ "$LOCALE" = "bg" ]; then
+    ng serve --configuration=bg --aot --host 0.0.0.0 --disable-host-check --disableHostCheck --base-href=$WEBCLIENT_BASE_HREF/ --public-host=$WEBCLIENT_PUBLIC_URL
+  else
+    ng serve --aot --host 0.0.0.0 --disable-host-check --disableHostCheck --base-href=$WEBCLIENT_BASE_HREF/ --public-host=$WEBCLIENT_PUBLIC_URL
+  fi
 fi
--- server.ts
+++ server.ts
@@ -1,14 +1,14 @@
 import 'zone.js/dist/zone-node';
-import {enableProdMode} from '@angular/core';
+import { enableProdMode } from '@angular/core';
 // Express Engine
-import {ngExpressEngine} from '@nguniversal/express-engine';
+import { ngExpressEngine } from '@nguniversal/express-engine';
 // Import module map for lazy loading
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
 var compress = require('compression');
 
-import {join} from 'path';
+import { join } from 'path';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 4200;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
@@ -63,15 +63,15 @@ router.get('*', (req, res) => {
   res.render(`index`, {
     req,
     providers: [
-      {provide: 'language', useFactory: () => locale, deps: []},
-      {provide: 'locale', useFactory: () => locale, deps: []},
-      {provide: 'ip', useFactory: () => ip, deps: []},
+      { provide: 'language', useFactory: () => locale, deps: [] },
+      { provide: 'locale', useFactory: () => locale, deps: [] },
+      { provide: 'ip', useFactory: () => ip, deps: [] },
 
     ]
   });
 });
 
-app.use('/saas_web', router);
+app.use(process.env.WEBCLIENT_BASE_HREF, router);
 
 // Start up the Node server
 app.listen(PORT, '0.0.0.0', () => {
